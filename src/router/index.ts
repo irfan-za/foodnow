46eedf69ba1ed66aec71610521c46fb2c23b059f
irfan-za <fanzahran123@gmail.com>
2025-06-20 22:35:26 +0700

    fix: not found route

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,7 +11,7 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/:pathMatch(.*)*',
+      path: '/:notFound',
       name: 'not-found',
       component: NotFoundView,
     },
